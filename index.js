43aa9983b7c559756f623dffe0f0aa51904a518a
Terry Lorber <terry@metatooth.com>
2021-02-08 12:33:41 -0500

    Setup to run on Heroku

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-require("./dist/server/main");
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || '8080';
+
+app.use(express.static('dist'));
+
+app.listen(port, () => {
+  console.log(`Herbert Client listing at #{port}`);
+});                                 
