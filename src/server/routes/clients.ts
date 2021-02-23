18ac4c6396f63bf3458d556fd8bdef3315b0ee82
Terry Lorber <terry@metatooth.com>
2021-02-23 19:07:49 +0000

    implement PUT /clients/:id

--- src/server/routes/clients.ts
+++ src/server/routes/clients.ts
@@ -5,13 +5,20 @@ import { query } from "../db";
 const router = Router();
 
 router.get("/", async (req, res) => {
-  const { rows } = await query("SELECT * FROM clients", []);
+  const { rows } = await query("SELECT * FROM clients c LEFT JOIN profiles p ON c.profile_id = p.id", []);
   res.status(200).json(rows);
 });
 
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
   const { rows } = await query("SELECT * FROM clients WHERE client = $1", [id]);
+  res.status(200).json(rows[0]);
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const pid = parseInt(req.query.profile_id as string);
+  const { rows } = await query("UPDATE clients SET profile_id = $1 WHERE client = $2", [pid, id]);
   res.status(200).json(rows[0]);
 });
 
