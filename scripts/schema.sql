52f92a24bd80e022b960bf2c972244d916801ffc
Terry Lorber <terry@metatooth.com>
2021-02-24 16:03:44 +0000

    add officespace

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -75,6 +75,7 @@ insert into device_types (device_type)
 insert into profiles (profile, lamp_start, lamp_duration, lamp_on_temperature,
        lamp_on_humidity, lamp_off_temperature, lamp_off_humidity)
        values
+       ('Officespace', '13:00', '10 hours', 18.35, 21, 12.85, 21),
        ('Torello Clone', '00:00', '24 hours', 22.8, 68, 22.8, 68),
        ('Torello Veg', '13:00', '18 hours', 25.3, 55, 23.1, 55),
        ('Torello Flower 1', '13:00', '12 hours', 24.7, 55, 21.1, 43),
