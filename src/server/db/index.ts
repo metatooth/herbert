33068c4a0121172533217a5d585376092ad5f7a3
Terry Lorber <terry@metatooth.com>
2021-03-17 12:15:48 -0400

    try ssl

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,9 +2,7 @@ import { Pool, Result } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: true
 });
 
 export async function query(text, params): Promise<Result> {
