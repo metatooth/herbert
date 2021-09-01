037e7bc9022377fd1e9d6ef1ea9d65b4233f300f
Terry Lorber <terry@metatooth.com>
2021-09-01 13:33:07 -0400

    add blue iris

--- scripts/kiosk.sh
+++ scripts/kiosk.sh
@@ -4,4 +4,4 @@ xset s off     # disable screen saver
 xset s noblank # don't blank the video device
 matchbox-window-manager -use_titlebar no &
 unclutter &    # hide X mouse cursor unless mouse activated
-chromium-browser --display=:0 --kiosk --incognito --window-position=0,0 https://milltownag.com,http://192.168.7.201
+chromium-browser --display=:0 --kiosk --incognito --window-position=0,0 http://192.168.7.50:81
