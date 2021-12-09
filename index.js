3bdec93fc12ba281f112133cc5415dcbdabe1d47
Terry Lorber <terry@metatooth.com>
2021-12-09 16:33:33 -0500

    Heroku sets up port forwarding to

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const role = process.env.ROLE || "client";
+const port = process.env.PORT;
 
 if (role === "client") {
   const express = require("express");
   const app = express();
-  const port = process.env.CLIENT_PORT || "8080";
+  const port = process.env.CLIENT_PORT || port;
 
   app.use(express.static("dist/client"));
 
   app.listen(port, () => {
     console.log(`Herbert ${role} listing at ${port}`);
   });
+} else if (role === "socket-server") {
+  process.env.API_PORT = port;
+  require("./dist/socket-server/main");
 } else {
+  process.env.API_PORT = port;
   require("./dist/server/main");
 }
