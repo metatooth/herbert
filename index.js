a9e0fdee4eeb8d35051298081fdca43f99cef951
Terry Lorber <terry@metatooth.com>
2021-12-09 17:34:28 -0500

    send that port

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const role = process.env.ROLE || "client";
-const port = process.env.PORT;
 
 if (role === "client") {
   const express = require("express");
   const app = express();
-  const port = process.env.CLIENT_PORT || port;
+  const port = process.env.CLIENT_PORT || process.env.PORT;
 
   app.use(express.static("dist/client"));
 
@@ -12,9 +11,9 @@ if (role === "client") {
     console.log(`Herbert ${role} listing at ${port}`);
   });
 } else if (role === "socket-server") {
-  process.env.API_PORT = port;
+  process.env.API_PORT = process.env.PORT;
   require("./dist/socket-server/main");
 } else {
-  process.env.API_PORT = port;
+  process.env.API_PORT = process.env.PORT;
   require("./dist/server/main");
 }
