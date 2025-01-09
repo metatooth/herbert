74223e2091a24a026598e745609599ee382cb908
Terry Lorber <terry@metatooth.com>
2025-01-08 22:26:35 -0500

    implement mock meter

--- src/worker/mock-meter.ts
+++ src/worker/mock-meter.ts
@@ -7,6 +7,9 @@ export class MockMeter extends Meter {
   }
 
   public status(): AnySocketMessage {
+    const now = new Date().getTime();
+    this.clime.temperature = 23.9 + 5 * Math.sin((2 * 3.14 * now) / 3600000);
+    this.clime.humidity = 0.55 + 0.05 * Math.cos((2 * 3.14 * now) / 3600000);
     return super.status();
   }
 }
